e5e922c99df77bc9e48e0e1771c0f8e481f05dd8
Heath Banner <God@Heaths-MacBook-Pro.local>
2019-05-01 17:29:42 -0400

    Implemented TweenMax and Smooth Scroll

---
+++ css/gsap.js
@@ -0,0 +1,5 @@
+import { TweenMax, Power2, TimelineLite } from "gsap/umd/TweenMax"; 
+import ScrollToPlugin from "gsap/umd/ScrollToPlugin"; 
+import Draggable from "gsap/umd/Draggable";
+
+TweenMax.to("#intro", 2, {x:200});
---
+++ gsap.js
@@ -0,0 +1,8 @@
+// import TweenMax from "/node_modules/gsap/TweenMax"
+
+TweenMax.from("#intro", 2, {css:{x:200, opacity:0}});
+TweenMax.from("nav", 2, {css:{x:-200, opacity:0}})
+TweenMax.from(".header2", 2, {css:{x:200, opacity:0}});
+TweenMax.from(".header2", 5, {css:{borderColor: 'none'}, delay: 2})
+TweenMax.from("#heathMirror", 2, {css:{x:-200, opacity:0}});
+TweenMax.staggerFrom('.sideNav', 1, {opacity:0, x:200, delay: 0.5}, 0.5)
