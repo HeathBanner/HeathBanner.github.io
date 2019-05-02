1746907ee7fe785898dd52673f7d11a491b32588
Heath Banner <God@Heaths-MBP.lan1>
2019-05-01 22:26:02 -0400

    Implemented new features on about page

--- gsap.js
+++ gsap.js
@@ -3,6 +3,6 @@
 TweenMax.from("#intro", 2, {css:{x:200, opacity:0}});
 TweenMax.from("nav", 2, {css:{x:-200, opacity:0}})
 TweenMax.from(".header2", 2, {css:{x:200, opacity:0}});
-TweenMax.from(".header2", 5, {css:{borderColor: 'none'}, delay: 2})
+TweenMax.from(".header2", 3, {css:{borderColor: 'none'}, delay: 2})
 TweenMax.from("#heathMirror", 2, {css:{x:-200, opacity:0}});
 TweenMax.staggerFrom('.sideNav', 1, {opacity:0, x:200, delay: 0.5}, 0.5)
